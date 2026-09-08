447e1b0879a328d95aa2ce3b8d7245b0ab68aef2
mahmoud34abc <mahmoud34abc@users.noreply.github.com>
2026-09-09 02:58:35 +0300

    "hmm.. how would i delete images without fs?"

--- code/discordbot.js
+++ code/discordbot.js
@@ -3,6 +3,7 @@ import { channel } from 'diagnostics_channel';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const path = require('path');
+const fs = require('fs');
 //const fs = require('node:fs');
 const { Client, GatewayIntentBits, EmbedBuilder, MessageEmbed, AttachmentBuilder } = require('discord.js');
 
